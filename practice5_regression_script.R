6481b640ddd7251eb9864b5bfa6d06fb520b27cb
Brooke Charbonneau <charbonb@uoguelph.ca>
2016-12-05 12:56:18 -0500

    commit

---
+++ practice5_regression_script.R
@@ -0,0 +1,41 @@
+library(tidyverse)
+library(apaTables)
+library(haven)
+library(predictionInterval)
+
+#load spss file
+my.data <- read_csv("reg_quiz2_data.csv")
+my.data
+str(my.data)
+
+#Correlation table
+apa.cor.table(my.data,filename="Table1.doc",table.number=1)
+
+
+#make sure data isn't curvilinear, so regression makes sense
+psych::pairs.panels(as.data.frame(my.data))
+
+
+## block regressionss
+#SE-PAS
+block1 = lm(aSuc~PAS,data=my.data)
+
+block2 = lm(aSuc~PAS+selfEsteem,data=my.data)
+
+apa.reg.table(block1, block2, filename="Table2.doc",table.number=2)
+
+#SE-NAS
+block3 = lm(aSuc~NAS,data=my.data)
+
+block4 = lm(aSuc~NAS+selfEsteem,data=my.data)
+
+apa.reg.table(block3, block4,filename="Table3.doc",table.number=3)
+
+#SE-PAS+NAS
+block5 = lm(aSuc~NAS+PAS,data=my.data)
+
+block6 = lm(aSuc~NAS+PAS+selfEsteem,data=my.data)
+
+apa.reg.table(block5, block6,filename="Table4.doc",table.number=4)
+
+
